9246d0fd81873a6e2772527eeeae00901e81f496
Денис <dultsevdv@gmail.com>
2024-03-12 22:14:49 +0500

    Регистратор репозиториев

---
+++ WPR.Repositories.EntityFramework/RepositoryRegistrator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using WPR.Repositories.Abstractions.Db;
+
+namespace WPR.Repositories.EntityFramework;
+
+public static class RepositoryRegistrator
+{
+    /// <summary>
+    /// Зарегистрировать интерфейсы репозиториев базы данных с реализацией EntityFramework
+    /// </summary>
+    public static IServiceCollection AddDbRepositories(this IServiceCollection services) => services
+        .AddScoped(typeof(IDbRepository<>), typeof(DbRepository<>))
+        .AddScoped(typeof(INamedDbRepository<>), typeof(DbNamedRepository<>))
+        .AddScoped(typeof(IDeletedDbRepository<>), typeof(DbDeletedRepository<>))
+        
+    ;
+}
